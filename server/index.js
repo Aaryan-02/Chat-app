f93e26e6192372fd9db5a25296cebe7eee15fab1
Aaryan <102778679+Aaryan-02@users.noreply.github.com>
2024-10-29 13:33:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,12 @@ const app = express();
 const socket = require("socket.io")
 require("dotenv").config();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://snappy-blue.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", userRoutes)
