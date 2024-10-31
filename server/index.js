d0859c3b64fa87b0c6d126532dcc6f222288bf8f
Aaryan <102778679+Aaryan-02@users.noreply.github.com>
2024-10-31 20:09:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,13 +7,7 @@ const app = express();
 const socket = require("socket.io")
 require("dotenv").config();
 
-app.use(cors({
-  origin: "https://vercel.live",
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-// app.options("*", cors());
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes)
@@ -26,6 +20,10 @@ mongoose.connect(process.env.MONGO_URL, {
     console.log("DB Connection Successfull");
 }).catch((err) => {
     console.log(err.message);
+});
+
+app.get("/", (req, res) => {
+  res.send("Running `Chat App` Server");
 });
 
 const server = app.listen(process.env.PORT, () => {
